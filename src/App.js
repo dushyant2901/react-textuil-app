531e84ead926a3a625cebc9d4252d73128141ee0
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-01-09 18:38:15 +0530

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
-import './App.css';
-
+import './App.css'
+import TextUtil from './component/TextUtil';
 function App() {
   return (
     <div className="App">
-      hello world
+     <TextUtil/>
     </div>
   );
 }
---
+++ src/component/TextUtil.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './Navbar';
+const TextUtil = () => {
+    return (
+        <>
+      <Navbar/>
+        </>
+    )
+}
+
+export default TextUtil
