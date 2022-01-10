a2d75927eb2fd054235c6d25a62b0871ea79f575
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-01-10 19:57:10 +0530

    nav refactored

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         <>
             <Navbar bg="dark" variant="dark">
     <Container>
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Navbar.Brand href="#home">TextUtil</Navbar.Brand>
     <Nav className="me-auto">
       <Nav.Link href="#home">Home</Nav.Link>
       <Nav.Link href="#features">Features</Nav.Link>
